1bd6a39c567ef3a235037282184853597a2ef1c4
Stefan <schadis@gmx.at>
2020-03-04 14:37:58 +0100

    added Taps for better overview

--- Settings.lua
+++ Settings.lua
@@ -51,27 +51,30 @@ function Hunter.Settings()
 								
     }
 
-
+	
 --  UI.AddHeader("DPS")
+	UI.AddTab("DPS")
 	UI.AddHeader("Offensiv")
 	UI.AddToggle("Aimed Shot", "Aimed Shot", true)
 	UI.AddToggle("Multi Shot", "Will use Multishot", true)	
 	UI.AddToggle("Arcane Shot", "Will use Arcanshot", true)
 	UI.AddToggle("Serpent Sting", "Will use Serpent Sting", true)
 	UI.AddToggle("Concussiv Shot", "Will use Concussiv Shot", true)
-    UI.AddRange	("Concussiv Shot Distance", "Range < yards to cast Concussiv Shot", 8, 45, 1, 30)
-    UI.AddToggle("Tranq Shot","Uses Tranq Shot if Enraged", true)
+    	UI.AddRange	("Concussiv Shot Distance", "Range < yards to cast Concussiv Shot", 8, 45, 1, 30)
+    	UI.AddToggle("Tranq Shot","Uses Tranq Shot if Enraged", true)
 	UI.AddToggle("Save Tranq Mana","Holds back 10% Mana for Tranqshot", true)
 	UI.AddDropdown("Tranq Order", "Tranqshot every first,second or third frenzy", { 1, 2, 3}, 1)
 	UI.AddToggle("Clipped Rotation", "Clipp Autoshots or not", true)    
 	
 --  Defensive
+	UI.AddTab("Defensive")
 	UI.AddHeader("Defensive")
 	UI.AddToggle("Aspect Of The Monkey", "Use Aspect of the Monkey", true)
 	UI.AddRange	("Seconds for PetAggro", "Seconds for PetAggro", 0, 3, 1, 0)	
 	UI.AddRange	("Aspect of the Monkey HP", "HP to cast AotM", 0, 100, 1, 75)
 	
---  Utility	
+--  Utility
+	UI.AddTab("Utility")
 	UI.AddHeader("Utility")
 	UI.AddToggle("HuntersMark", "HuntersMark", true)
 	UI.AddToggle("TrueShot Buff", "TrueShot Buff", true)
@@ -86,16 +89,20 @@ function Hunter.Settings()
 	UI.AddBlank()
 	UI.AddRange	("Mend Pet HP", "Pet HP to cast Mend Pet", 0, 60, 1, 35)	
 	UI.AddRange	("Send Pet back in", "only works with Pullback", 61, 100, 1, 70)
-    --Consumables Health
+    
+	
+	--Consumables Health
+	UI.AddTab("Consumables")
 	UI.AddHeader("Consumables")
 	UI.AddToggle("Use Best HP Potion", "Check back for Potions and use best available one")
 	UI.AddRange("Use Potion at #% HP", nil, 10, 100, 1, 50, true)
 
-    --Consumables Mana
+    	--Consumables Mana
+	
 	UI.AddHeader("Mana Potions on Boss Targets")
 	UI.AddToggle("Use Best Mana Potion", "Check back for Potions and use best available one") 
 	UI.AddRange("Use Potion at #% Mana", nil, 10, 100, 1, 50, true)
-    UI.AddToggle("Use Demonic or Dark Rune", "Uses Demonic or Dark Rune " ) 	
+    	UI.AddToggle("Use Demonic or Dark Rune", "Uses Demonic or Dark Rune " ) 	
 	UI.AddRange("Use Rune at #% Mana", nil, 10, 100, 1, 50, true)
    
 
